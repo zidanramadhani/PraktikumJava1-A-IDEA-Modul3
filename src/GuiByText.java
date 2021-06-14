22cd667fb14a020ab597c6963d5818f7b32267c4
zidanoke <zidanramadhani007@gmail.com>
2021-06-14 22:52:16 +0800

    Commit Pertama

---
+++ src/GuiByText.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GuiByText extends JFrame {
+    GuiByText(){
+        setSize(640, 480);
+    }
+
+    public static void main(String[] args) {
+        GuiByText guiByText = new GuiByText();
+        guiByText.setVisible(true);
+    }
+}
