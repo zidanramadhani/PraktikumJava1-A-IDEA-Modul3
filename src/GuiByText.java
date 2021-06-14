d1f4686cca95a28845d94acba1b3414109ee3131
zidanoke <zidanramadhani007@gmail.com>
2021-06-14 23:15:57 +0800

    Menambahkan dan Menampilkan Komponen

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 
 public class GuiByText extends JFrame {
+
+    JLabel label1 = new JLabel("Coba Input 1");
+    JTextField textField1 = new JTextField();
+
     GuiByText(){
         setSize(640, 480);
         setLocationRelativeTo(null);
@@ -8,8 +12,19 @@ public class GuiByText extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    void meletekkanKomponen(){
+        getContentPane().setLayout(null);
+
+        getContentPane().add(label1);
+        getContentPane().add(textField1);
+
+        label1.setBounds(16,32,100,25);
+        textField1.setBounds(124,32,100,25);
+    }
+
     public static void main(String[] args) {
         GuiByText guiByText = new GuiByText();
+        guiByText.meletekkanKomponen();
         guiByText.setVisible(true);
     }
 }
