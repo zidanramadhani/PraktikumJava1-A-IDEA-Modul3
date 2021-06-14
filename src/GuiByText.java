76f2434b63b5f1eecde36058b5b1a9402428e053
zidanoke <zidanramadhani007@gmail.com>
2021-06-14 23:01:19 +0800

    Menampilkan Frame Pertama Kali

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -3,6 +3,9 @@ import javax.swing.*;
 public class GuiByText extends JFrame {
     GuiByText(){
         setSize(640, 480);
+        setLocationRelativeTo(null);
+        setTitle("Gui By Text");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
